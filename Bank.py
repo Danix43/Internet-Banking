7789d30ad66a98b64eb7014dd453fb004e900f23
Danix43 <daninitu39@gmail.com>
2025-04-03 21:25:13 +0300

    Update

--- Bank.py
+++ Bank.py
@@ -6,8 +6,9 @@ class User:
     def __init__(self, name, initial_balance):
         self.name = name
         self.balance = initial_balance
-        self.iban = name + str(rnd.random())
-        self.__logs = dict()
+        # self.iban = name + str(rnd.random())
+        self.iban = name
+        self.__logs = tuple()
 
     def set_logs(self, logs):
         self.__logs = logs
@@ -18,6 +19,18 @@ class User:
     def print_logs(self):
         for i, j in self.get_logs():
             print(f"key {i}: {j}")
+
+    def add_transaction(self, transaction):
+        self.set_logs((*self.get_logs(), transaction))
+
+    def export_acc(self):
+        return {
+            self.iban,
+            (self.name, self.balance, self.get_logs()),
+        }
+
+    def __str__(self):
+        return f"This is {self.name} \n Details: \n Current balance: {self.balance} \n IBAN: {self.iban}"
 
 
 class Transaction:
@@ -36,6 +49,32 @@ class Bank:
         self.registered_users = []
         self.bank_logs = []
 
+    def retrieve_users(self, path):
+        """
+        Should read the csv file that contains the user informations and load them into registered users list
+
+        Update 1: Use pickle
+        """
+        pass
+
+    def save_users(self, path):
+        pass
+
+    def create_user(self, name, initial_deposit):
+        new_user = User(name, initial_deposit)
+        self.registered_users.append(new_user)
+        print(f"Made a new account \n {new_user}")
+        return new_user
+
+    def find_user_by_iban(self, iban):
+        for usr in self.registered_users:
+            if usr.iban == iban:
+                print("User found")
+                return usr
+            else:
+                print("User not registered")
+                return None
+
     def send_money(self, sender, receiver, amount):
         # not enoutf money
         if sender.balance < amount:
@@ -44,7 +83,10 @@ class Bank:
             # enoutg money
             receiver.balance += amount
             sender.balance -= amount
-            self.bank_logs.append(Transaction(sender, receiver, amount))
+            trans = Transaction(sender, receiver, amount)
+            self.bank_logs.append(trans)
+            sender.add_transaction(trans)
+            receiver.add_transaction(trans)
 
     def print_bank_logs(self):
         for log in self.bank_logs:
--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from Bank import User, Bank
-
-user1 = User("user1", 50)
-user2 = User("user2", 30)
+import pickle
 
 bank = Bank()
 
-bank.send_money(user1, user2, 20)
-bank.send_money(user1, user2, 20)
-bank.send_money(user1, user2, 20)
+user1 = bank.create_user("user1", 50)
+user2 = bank.create_user("user2", 20)
 
-bank.print_bank_logs()
+print("")
+print("")
+print("")
+print("")
+
+
+bank.send_money(user1, user2, 20)
+bank.send_money(user1, user2, 10)
+
+# bank.print_bank_logs()
+
+# print(user1.export_acc())
+# print("")
+# print(user2.export_acc())
+
+with open("bank_details.pkl", "wb") as f:
+    pickle.dump(user1.export_acc(), f)
+f.close()
+
+with open("bank_details.pkl", "rb") as f:
+    loaded_info = pickle.load(f)
+    print(loaded_info)
+
+
+# bank.send_money(user1, user2, 20)
+# bank.send_money(user1, user2, 20)
+# bank.send_money(user1, user2, 20)
+
+# bank.print_bank_logs()
